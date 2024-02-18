4c51e2b9f38180a4edf4dcc739e4afc207f79359
Scott Downing <scott.r.downing@gmail.com>
2024-02-18 10:04:50 +0100

    Migrate to new style starter

--- source/SmartFriends.Host/Program.cs
+++ source/SmartFriends.Host/Program.cs
@@ -1,23 +1,36 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using SmartFriends.Api;
+using SmartFriends.Api.JsonConvertes;
+using SmartFriends.Api.Models;
+using SmartFriends.Mqtt;
+using SmartFriends.Mqtt.Models;
 
-namespace SmartFriends.Host
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConsole().AddDebug();
+
+builder.Services.Configure<Configuration>(builder.Configuration.GetSection("SmartFriends"));
+builder.Services.Configure<MqttConfiguration>(builder.Configuration.GetSection("Mqtt"));
+builder.Services.AddSingleton(x => new Session(x.GetService<IOptions<Configuration>>().Value, x.GetService<ILogger<Session>>()));
+builder.Services.AddSingleton(x => new TypeTemplateEngine(x.GetService<IOptions<MqttConfiguration>>()?.Value));
+builder.Services.AddSingleton(x => new MqttClient(x.GetService<IOptions<MqttConfiguration>>()?.Value, x.GetService<ILogger<MqttClient>>(), x.GetService<TypeTemplateEngine>()));
+builder.Services.AddHostedService(x => x.GetService<Session>());
+builder.Services.AddHostedService<MqttClientService>();
+builder.Services.AddControllers().AddNewtonsoftJson(x =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    x.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+    x.SerializerSettings.Formatting = Formatting.Indented;
+    x.SerializerSettings.Converters = new List<JsonConverter> { new FuzzyValueConverter() };
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(builder =>
-                {
-                    builder.AddConsole();
-                    builder.AddDebug();
-                })
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+app.MapControllers();
+app.Run();
